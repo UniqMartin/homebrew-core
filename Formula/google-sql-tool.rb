c6bc939d8032c801c875e084040e0563b554ba23
Martin Afanasjew <martin@afanasjew.de>
2016-07-11 01:01:32 +0200

    google-sql-tool: fix code style issues

--- Formula/google-sql-tool.rb
+++ Formula/google-sql-tool.rb
@@ -11,10 +11,10 @@ class GoogleSqlTool < Formula
     # Patch script to find jar
     chmod 0755, "google_sql.sh"
     inreplace "google_sql.sh",
-      'SQL_SH_DIR="$(cd $(dirname $0); pwd)"',
+      "SQL_SH_DIR=\"$(cd $(dirname $0); pwd)\"",
       "SQL_SH_DIR=\"#{libexec}\""
 
-    libexec.install ["google_sql.sh", "google_sql.jar"]
-    bin.install_symlink libexec+("google_sql.sh") => "google_sql"
+    libexec.install %w[google_sql.sh google_sql.jar]
+    bin.install_symlink libexec/"google_sql.sh" => "google_sql"
   end
 end

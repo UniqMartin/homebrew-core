694fd77ba55274f68cf554055fc3598e817cc57a
Martin Afanasjew <martin@afanasjew.de>
2016-07-11 01:06:32 +0200

    h2: fix code style issues

--- Formula/h2.rb
+++ Formula/h2.rb
@@ -23,13 +23,13 @@ class H2 < Formula
     #
     # https://github.com/h2database/h2database/issues/218
     h2_script = File.read("bin/h2.sh").gsub("\r\n", "\n")
-    File.open("bin/h2.sh", "w") {|f| f.write h2_script}
+    File.open("bin/h2.sh", "w") { |f| f.write h2_script }
 
     # Fix the permissions on the script
     chmod 0755, "bin/h2.sh"
 
     libexec.install Dir["*"]
-    (bin+"h2").write script
+    (bin/"h2").write script
   end
 
   plist_options :manual => "h2"
